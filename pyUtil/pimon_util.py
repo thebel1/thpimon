8019fc74cccc1c70adbb295c316020de81d067af
Thomas Dupuy <thom4s.d@gmail.com>
2021-08-12 14:06:37 -0400

    Pepified.

--- pyUtil/pimon_util.py
+++ pyUtil/pimon_util.py
@@ -3,6 +3,7 @@
 import sys
 from pimonLib import *
 
+
 def main(argv):
     pimon = PiMon()
     boardMACBytes = pimon.getBoardMAC().to_bytes(6, 'little')
@@ -19,6 +20,7 @@ def main(argv):
     print('Board MAC Address:\t{}'.format(boardMACStr))
     print('Board Serial:\t\t{0:#0{1}x}'.format(pimon.getBoardSerial(), 16))
     print('Temp:\t\t\t{:.1f} (deg. C)'.format(pimon.getTemp()))
+
 
 if __name__ == '__main__':
     main(sys.argv)
